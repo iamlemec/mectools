1abcd70c222c60be898def9503b290435cb496c7
Douglas Hanley <thesecretaryofwar@gmail.com>
2016-08-23 14:49:01 -0400

    db tools start

---
+++ db_tools.py
@@ -0,0 +1,64 @@
+# database tools (sqlite3)
+
+import sqlite3
+
+def tables(cur):
+    ret = cur.execute('select name from sqlite_master where type=\'table\'')
+    print('\n'.join([tn for (tn,) in ret.fetchall()]))
+
+def table_info(cur,name):
+    ret = cur.execute('pragma table_info(\'%s\')' % name)
+    print('\n'.join(['{:<10s} {:s}'.format(t,n) for (i,n,t,_,_,_) in ret.fetchall()]))
+
+def table_op(tname,schema):
+    def wrap(f):
+        def f1(**kwargs):
+            if 'db' in kwargs:
+                con = sqlite3.connect(kwargs.pop('db'))
+                close = True
+            else:
+                con = kwargs.pop('con')
+                close = False
+            if 'cur' in kwargs:
+                cur = kwargs.pop('cur')
+            else:
+                cur = con.cursor()
+
+            if kwargs.pop('clobber',False):
+                cur.execute('drop table if exists %s' % tname)
+            cur.execute('create table if not exists %s as %s' % (tname,schema))
+
+            f(cur,**kwargs)
+
+            con.commit()
+            if close:
+                con.close()
+        return f1
+    return wrap
+
+class ChunkInserter:
+    def __init__(self,con,cur,cmd,chunk_size=1000,output=True):
+        self.con = con
+        self.cur = cur
+        self.cmd = cmd
+        self.chunk_size = chunk_size
+        self.output = output
+        self.items = []
+        self.i = 0
+
+    def insert(self,*args):
+        self.items.append(args)
+        if len(self.items) >= self.chunk_size:
+            self.commit()
+            return True
+        else:
+            return False
+
+    def commit(self):
+        self.i += 1
+        if self.output:
+            print('Committing chunk %d (%d)' % (self.i,len(self.items)))
+        self.cur.executemany(self.cmd,self.items)
+        self.con.commit()
+        self.items = []
+
