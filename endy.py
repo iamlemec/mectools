869179f617a62d89c8b2d3fe2b2241d4ee7fef1d
Douglas Hanley <thesecretaryofwar@gmail.com>
2018-03-16 08:34:25 -0400

    true project

--- endy.py
+++ endy.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 # digitize nd array
-def digitize_nd(x, bins):
+def digitize(x, bins):
     N = len(bins)
     d = np.zeros_like(x, dtype=np.int)
     for i in range(N):
@@ -11,23 +11,23 @@ def digitize_nd(x, bins):
     return d
 
 # choose along a given axis
-def choose_nd(d, f, axis=0):
+def choose(d, f, axis=0):
     return np.choose(d.swapaxes(0, axis), f.swapaxes(0, axis)).swapaxes(0, axis)
 
 # interpolate non-flat domains on a given axis
-def interp_nd(x1, x0, f0, axis=0):
+def interp(x1, x0, f0, axis=0):
     N = len(x0)
-    d1 = digitize_nd(x1, x0)
+    d1 = digitize(x1, x0)
     d2 = np.minimum(N-1, d1+1)
     d1[d1==d2] -= 1 # only happens on top bin
     q = (x1-x0[d1])/(x0[d2]-x0[d1])
-    g1 = choose_nd(d1, f0, axis=axis)
-    g2 = choose_nd(d2, f0, axis=axis)
+    g1 = choose(d1, f0, axis=axis)
+    g2 = choose(d2, f0, axis=axis)
     f1 = (1-q)*g1 + q*g2
     return f1
 
 # insert added possibly flat dimensions
-def expand_nd(x, dims=1, axis=-1):
+def expand(x, dims=1, axis=-1):
     if type(dims) is int:
         dims = (1,)*dims
     if axis < 0:
@@ -36,9 +36,20 @@ def expand_nd(x, dims=1, axis=-1):
     return x.reshape(shape)
 
 # project non-flat ranges on bins
-def project_nd(xlo, xhi, bins):
-    blo, bhi = expand_nd(bins[:-1], xlo.ndim, 0), expand_nd(bins[1:], xhi.ndim, 0)
-    xlo, xhi = expand_nd(xlo, 1, -1), expand_nd(xhi, 1, -1)
+def project(bins, xp=None, xlo=None, xhi=None, axis=-1):
+    # if point given, assume additively scaled ranges around each bin
+    if xp is not None:
+        nd = len(xp.shape)
+        db = np.diff(bins)
+        dbx = expand(expand(db, nd-axis-1, -1), axis, 0)
+        xlo, xhi = xp - dbx/2, xp + dbx/2
+
+    # general code for xlo, xhi
+    blo, bhi = expand(bins[:-1], xlo.ndim, 0), expand(bins[1:], xhi.ndim, 0)
+    xlo, xhi = expand(xlo, 1, -1), expand(xhi, 1, -1)
     upper = np.maximum(0, bhi - np.maximum(xlo, blo))
     lower = np.maximum(0, bhi - np.maximum(xhi, blo))
-    return (upper-lower)/(bhi-blo)
+    total = upper - lower
+    total[...,[ 0]] += np.maximum(0, blo[..., 0] - xlo) - np.maximum(0, blo[..., 0] - xhi)
+    total[...,[-1]] += np.maximum(0, xhi - bhi[...,-1]) - np.maximum(0, xlo - bhi[...,-1])
+    return total/(bhi-blo)
