93015363b2e9bde05d87fb49c079ede7cd039d4a
Douglas Hanley <thesecretaryofwar@gmail.com>
2018-08-29 13:21:12 -0400

    clip fix

--- clip.py
+++ clip.py
@@ -2,11 +2,11 @@ import json
 import redis
 
 class Clip:
-    def __init__(self):
-        self.__dict__['red'] = redis.Redis(host='localhost', port=6379, db=0)
+    def __init__(self, host='localhost', pot=6379):
+        self.__dict__['redis'] = redis.Redis(host=host, port=port, db=0)
 
     def __setattr__(self, key, value):
-        self.red[key] = json.dumps(value)
+        self.redis[key] = json.dumps(value)
 
     def __getattr__(self, key):
-        return json.loads(self.red[key])
+        return json.loads(self.redis[key])
