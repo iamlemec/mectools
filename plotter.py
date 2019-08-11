e96d442d1de63f64c35e3cf35bece9cf767ae9b4
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-08-10 20:46:36 -0400

    altair plotter

--- plotter.py
+++ plotter.py
@@ -23,29 +23,30 @@ def plotter(pyplot=True, backend='GTK3Agg', theme=['clean'], rc={}):
         return plt
 
 # define the theme by returning the dictionary of configurations
-def altair(size=14):
-    def theme():
-        return {
-            'config': {
-                'view': {
-                    'height': 300,
-                    'width': 400,
-                    'strokeWidth': 0
-                },
-                'axis': {
-                    'grid': False,
-                    'domainColor': 'black',
-                    'tickColor': 'black',
-                    'titleFontSize': size,
-                    'labelFontSize': size
-                },
-                'legend': {
-                    'titleFontSize': size,
-                    'labelFontSize': size
-                }
+def altair_config(size=14, width=400, height=300, glob=True):
+    theme = {
+        'config': {
+            'view': {
+                'height': height,
+                'width': width,
+                'strokeWidth': 0
+            },
+            'axis': {
+                'grid': False,
+                'domainColor': 'black',
+                'tickColor': 'black',
+                'titleFontSize': size,
+                'labelFontSize': size
+            },
+            'legend': {
+                'titleFontSize': size,
+                'labelFontSize': size
             }
         }
+    }
     import altair as alt
-    alt.themes.register('mec', theme)
-    alt.themes.enable('mec')
-    return alt
+    if glob:
+        alt.themes.register('mec', lambda: theme)
+        alt.themes.enable('mec')
+    else:
+        return theme
