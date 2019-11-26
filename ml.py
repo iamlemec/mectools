1468fa70ecf4cbe6b089d0fed8599cb19a71cc15
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-11-25 22:55:59 -0600

    factor out design matrix

--- ml.py
+++ ml.py
@@ -16,12 +16,13 @@ def frame_eval(exp, data, engine='pandas'):
     elif engine == 'python':
         return eval(exp, globals(), data)
 
-def vstack(v, N=1):
+def vstack(v, N=None):
     if len(v) == 0:
         return np.array([[]]).reshape((0, N))
     else:
         return np.vstack(v)
 
+# this assumes row major to align wiht product
 def strides(v):
     if len(v) == 1:
         return np.array([1])
@@ -34,105 +35,82 @@ def prefix(p, v):
 def chainer(v):
     return list(chain.from_iterable(v))
 
-# need to have all features list -> onehot
-# map terms -> feature lists
-# terms becomes power sums of features -> then one hot again
+def design_matrix(x, data, N=None):
+    return vstack([frame_eval(z, data) for z in x], N).T
 
-# high dimensional fixed effects - expects strings or expressions
-def sparse_ols_simple(y, x=[], fe=[], data=None, intercept=True, drop='first'):
-    # generate dense input variables
-    y_vec = frame_eval(y, data)
-    N = len(y_vec)
-    x_mat = vstack([frame_eval(z, data) for z in x], N).T
-    fe_mat = vstack([frame_eval(z, data) for z in fe], N).T
-
-    # add intercept if needed
-    if intercept:
-        x_mat = np.hstack([np.ones((N, 1)), x_mat])
-        x = ['intercept'] + x
-
-    # sparse one-hot fixed effects
-    if len(fe) == 0:
-        fe_spmat = sp.coo_matrix((N, 0))
-        fe_names = []
-    else:
-        hot = OneHotEncoder(categories='auto', drop=drop)
-        fe_spmat = hot.fit_transform(fe_mat)
-        fe_names = hot.get_feature_names(fe).tolist()
-
-    # compute coefficients
-    x_spmat = sp.hstack([x_mat, fe_spmat])
-    beta = sp.linalg.spsolve(x_spmat.T*x_spmat, x_spmat.T*y_vec)
-
-    # match names
-    names = x + fe_names
-    ret = pd.Series(dict(zip(names, beta)))
-
-    return ret
-
-# high dimensional fixed effects - expects strings or expressions
-# can have tuples of strings in fixed effects
-def sparse_ols_cross(y, x=[], fe=[], data=None, intercept=True, drop='first'):
-    # generate output variables
-    y_vec = frame_eval(y, data)
-    N = len(y_vec)
-
-    # generate input variables
-    x_mat = vstack([frame_eval(z, data) for z in x], N).T
-    if intercept:
-        x_mat = np.hstack([np.ones((N, 1)), x_mat])
-        x = ['intercept'] + x
+def sparse_categorical(terms, data, N=None, drop='first'):
+    if len(terms) == 0:
+        return sp.coo_matrix((N, 0)), []
 
     # generate map between terms and features
-    fe = [[z] if type(z) is str else z for z in fe]
-    feat = chainer(fe)
-    feat_mat = vstack([frame_eval(z, data) for z in feat], N).T
-    term_tag = [':'.join(term) for term in fe]
-    term_map = [[feat.index(z) for z in term] for term in fe]
+    terms = [[z] if type(z) is str else z for z in terms]
+    feats = chainer(terms)
+    feat_mat = design_matrix(feats, data, N=N)
+    term_tags = [':'.join(t) for t in terms]
+    term_map = [[feats.index(z) for z in t] for t in terms]
 
     # ordinally encode fixed effects
     enc_ord = OrdinalEncoder(categories='auto')
     feat_ord = enc_ord.fit_transform(feat_mat).astype(np.int)
-    feat_name = [z.astype(str) for z in enc_ord.categories_]
-    feat_size = [len(z) for z in enc_ord.categories_]
+    feat_names = [z.astype(str) for z in enc_ord.categories_]
+    feat_sizes = [len(z) for z in enc_ord.categories_]
 
-    if len(fe) == 0:
-        final_spmat = sp.coo_matrix((N, 0))
-        final_name = []
+    # generate cross-matrices and cross-names
+    form_vals = []
+    form_names = []
+    for term_idx in term_map:
+        # generate cross matrices
+        term_sizes = [feat_sizes[i] for i in term_idx]
+        term_strides = strides(term_sizes)
+        cross_vals = feat_ord[:,term_idx].dot(term_strides)
+        form_vals.append(cross_vals)
+
+        # generate cross names
+        term_names = [feat_names[i] for i in term_idx]
+        cross_names = [':'.join(x) for x in product(*term_names)]
+        form_names.append(cross_names)
+
+    # one hot encode all (cross)-terms
+    hot = OneHotEncoder(categories='auto', drop=drop)
+    final_mat = vstack(form_vals).T
+    final_spmat = hot.fit_transform(final_mat)
+
+    # find all cross-term names
+    if hot.drop_idx_ is None:
+        seen_cats = hot.categories_
     else:
-        form_val = []
-        form_name = []
-        for term_idx in term_map:
-            # generate cross ordinals
-            term_size = [feat_size[i] for i in term_idx]
-            term_stride = strides(term_size)
-            cross_val = feat_ord[:,term_idx].dot(term_stride)
-            form_val.append(cross_val)
+        seen_cats = [np.delete(c, i) for c, i in zip(hot.categories_, hot.drop_idx_)]
+    seen_names = [[n[i] for i in c] for c, n in zip(seen_cats, form_names)]
+    final_names = chainer(prefix(f'{t}=', n) for t, n in zip(term_tags, seen_names))
 
-            # generate cross names
-            term_name = [feat_name[i] for i in term_idx]
-            cross_name = np.array([':'.join(x) for x in product(*term_name)])
-            form_name.append(cross_name)
+    return final_spmat, final_names
 
-        # one hot encode all (cross)-terms
-        hot = OneHotEncoder(categories='auto', drop=drop)
-        final_mat = vstack(form_val).T
-        final_spmat = hot.fit_transform(final_mat)
-        if hot.drop_idx_ is None:
-            seen_cats = hot.categories_
-        else:
-            seen_cats = [np.delete(c, i) for c, i in zip(hot.categories_, hot.drop_idx_)]
-        seen_name = [n[c] for c, n in zip(seen_cats, form_name)]
-        final_name = chainer(prefix(f'{t}=', n) for t, n in zip(term_tag, seen_name))
+## high dimensional fixed effects
+# x expects strings or expressions
+# fe can have strings or tuples of strings
+def sparse_ols(y, x=[], fe=[], data=None, intercept=True, drop='first'):
+    # generate output variables
+    y_vec = frame_eval(y, data)
+    N = len(y_vec)
 
+    # find dense variable matrix
+    x_mat = design_matrix(x, data, N=N)
+
+    # find sparse categorical matrix
+    fe_spmat, fe_names = sparse_categorical(fe, data, N=N, drop=drop)
+
+    # optionally add intercept
+    if intercept:
+        x_mat = np.hstack([np.ones((N, 1)), x_mat])
+        x = ['intercept'] + x
 
     # compute coefficients
-    x_spmat = sp.hstack([x_mat, final_spmat])
-    beta = sp.linalg.spsolve(x_spmat.T*x_spmat, x_spmat.T*y_vec)
+    x_spmat = sp.hstack([x_mat, fe_spmat])
+    betas = sp.linalg.spsolve(x_spmat.T*x_spmat, x_spmat.T*y_vec)
 
     # match names
-    name = x + final_name
-    ret = pd.Series(dict(zip(name, beta)))
+    names = x + fe_names
+    ret = pd.Series(dict(zip(names, betas)))
 
     return ret
 
